8f53f4fc9ae15c3ead9f128aa7ecd0c4d8dd7f42
justynaMak <justynamakuch@outlook.com>
2020-01-09 13:48:02 +0100

    zmiana wyglądu aplikacji

--- src/app.js
+++ src/app.js
@@ -1,191 +1,202 @@
+// import "../node_modules/@fortawesome/fontawesome-free/css/all";
+
+import "../node_modules/@fortawesome/fontawesome-free/css/all.min.css";
+import "./css/style.css";
+import "../node_modules/@fortawesome/fontawesome-free/js/all";
+
 import NewTask from "./js/new-task";
 import week from "./js/week";
 
 import {
-    findActiveDay,
-    selectDay,
-    generateHtml,
-    updateDataId,
-    checkedTask,
-    showTasksCount,
-    showDate,
-    addDateToWeekObj,
-    clearAllTasks
+  findActiveDay,
+  selectDay,
+  generateHtml,
+  updateDataId,
+  checkedTask,
+  showTasksCount,
+  showDate,
+  addDateToWeekObj,
+  clearAllTasks
 } from "./js/helpers";
 
-(function () {
-    const toDoList = document.querySelector("#to-do-list");
-    const btnRemoveAll = document.querySelector(".btn-clear-all");
-    const navItems = [...document.querySelectorAll(".nav-item")];
+(function() {
+  const toDoList = document.querySelector("#to-do-list");
+  const btnRemoveAll = document.querySelector(".btn-clear-all");
+  const navItems = [...document.querySelectorAll(".nav-item")];
 
-    const updateData = function () {
-        localStorage.setItem("week", JSON.stringify(week));
-    };
+  const updateData = function() {
+    localStorage.setItem("week", JSON.stringify(week));
+  };
 
+  const chooseDay = function(e) {
+    const day = e.currentTarget.dataset.day;
+    const items = [...document.querySelectorAll(".single-task:not(#clone)")];
+    selectDay(day, navItems);
+    const dayActive = findActiveDay(week);
+    week[dayActive].active = false;
+    week[day].active = true;
 
-    const chooseDay = function (e) {
-        const day = e.currentTarget.dataset.day;
-        const items = [...document.querySelectorAll(".single-task:not(#clone)")];
-        selectDay(day, navItems);
-        const dayActive = findActiveDay(week);
-        week[dayActive].active = false;
-        week[day].active = true;
-
-        if (!week[day].tasks.length) {
-            btnRemoveAll.style.display = "none";
-        }
-
-        items.forEach(item => {
-            toDoList.removeChild(item);
-        });
-        week[day].tasks.forEach(task => {
-            generateHtml(task);
-            if (task.done) {
-                checkedTask(task);
-            }
-        });
-        updateData();
-
-    };
-
-    const showContent = function () {
-        const currentDay = showDate(navItems, week);
-        selectDay(currentDay, navItems);
-        week[currentDay].active = true;
-
-        const localSt = localStorage.getItem("week");
-        const data = JSON.parse(localSt);
-        clearAllTasks(data, navItems);
-        addDateToWeekObj(week, navItems);
-
-        if (localStorage.getItem("week")) {
-            for (let key in data) {
-                data[key].tasks.forEach(day => {
-                    const elem = new NewTask();
-                    elem.setTask(day.task);
-                    elem.setDone(day.done);
-                    elem.setId(day.id);
-                    week[key].addTask(elem);
-                });
-            }
-            data[currentDay].tasks.forEach(task => {
-                generateHtml(task);
-
-                if (task.done) {
-                    checkedTask(task);
-                }
-            });
-        }
-        showTasksCount(week);
-        updateData();
-    };
-
-    const addNewTask = function (e) {
-        e.preventDefault();
-        const day = findActiveDay(week);
-
-        if (week[day].tasks.length >= 10) {
-            alert("Maksymalna ilość zadań to 10. Jeśli chcesz dodać nowe zadanie musisz coś usunąć");
-            return;
-        }
-
-        function getValue() {
-            const input = document.querySelector(".input-add");
-            let inputValue = input.value;
-
-            if (inputValue === "") return;
-            input.value = "";
-            return inputValue;
-        }
-        const value = getValue();
-        let task;
-
-        if (value) {
-            task = new NewTask();
-            task.setTask(value);
-            task.setId(week[day].addTask(task));
-            generateHtml(task);
-            updateData();
-            showTasksCount(week);
-            $('#toast-added').toast('show');
-        }
-    };
-
-    const removeTask = function (e) {
-        e.preventDefault();
-        if (e.target.classList.contains("delete-btn") || e.target.parentElement.classList.contains("delete-btn")) {
-            const element = e.target.closest("li");
-            const day = findActiveDay(week);
-            week[day].removeTask(element.dataset.id);
-
-            element.parentElement.removeChild(element);
-
-            $('#toast-removed').toast('show');
-
-            week[day].tasks.forEach((task, i) => {
-                task.setId(i);
-            });
-
-            updateDataId(week[day].tasks);
-            updateData();
-            showTasksCount(week);
-
-            if (!week[day].tasks.length) {
-                btnRemoveAll.style.display = "none";
-            }
-
-        }
-    };
-
-    const checkTask = function (e) {
-        e.preventDefault();
-        if (e.target.classList.contains("check-btn") || e.target.parentElement.classList.contains("check-btn")) {
-            const element = e.target.closest("li");
-            const day = findActiveDay(week);
-            week[day].tasks[element.dataset.id].setDone(true);
-            if (e.target.classList.contains("check-btn")) e.target.classList.add("btn-success");
-            else e.target.parentElement.classList.add("btn-success");
-            updateData();
-            showTasksCount(week);
-        }
-    };
-
-    const updateTask = function (e) {
-        e.preventDefault();
-        if (e.target.classList.contains("task-field")) {
-            const currentTaskId = e.target.closest("li").dataset.id;
-            const currentValue = e.target.value;
-            const day = findActiveDay(week);
-            week[day].tasks[currentTaskId].setTask(currentValue);
-            updateData();
-        }
+    if (!week[day].tasks.length) {
+      btnRemoveAll.style.display = "none";
     }
 
-    const removeAllTasks = function (e) {
-        e.preventDefault();
-        const day = findActiveDay(week);
-        week[day].removeAllTasks();
-        updateData();
-
-        const elems = document.querySelectorAll('.single-task:not(#clone)');
-        elems.forEach(elem => {
-            toDoList.removeChild(elem);
-        });
-        if (!(week[day].tasks.length)) {
-            btnRemoveAll.style.display = "none";
-        }
-        showTasksCount(week);
-        $('#toast-removed-all').toast('show');
-    };
-
-    document.addEventListener("DOMContentLoaded", showContent);
-    document.querySelector("#btn-add").addEventListener("click", addNewTask);
-    toDoList.addEventListener("click", removeTask);
-    toDoList.addEventListener("click", checkTask);
-    toDoList.addEventListener("input", updateTask);
-    btnRemoveAll.addEventListener("click", removeAllTasks);
-    navItems.forEach(item => {
-        item.addEventListener("click", chooseDay);
+    items.forEach(item => {
+      toDoList.removeChild(item);
     });
+    week[day].tasks.forEach(task => {
+      generateHtml(task);
+      if (task.done) {
+        checkedTask(task);
+      }
+    });
+    updateData();
+  };
 
+  const showContent = function() {
+    const currentDay = showDate(navItems, week);
+    selectDay(currentDay, navItems);
+    week[currentDay].active = true;
+
+    const localSt = localStorage.getItem("week");
+    const data = JSON.parse(localSt);
+    clearAllTasks(data, navItems);
+    addDateToWeekObj(week, navItems);
+
+    if (localStorage.getItem("week")) {
+      for (let key in data) {
+        data[key].tasks.forEach(day => {
+          const elem = new NewTask();
+          elem.setTask(day.task);
+          elem.setDone(day.done);
+          elem.setId(day.id);
+          week[key].addTask(elem);
+        });
+      }
+      data[currentDay].tasks.forEach(task => {
+        generateHtml(task);
+
+        if (task.done) {
+          checkedTask(task);
+        }
+      });
+    }
+    showTasksCount(week);
+    updateData();
+  };
+
+  const addNewTask = function(e) {
+    e.preventDefault();
+    const day = findActiveDay(week);
+
+    if (week[day].tasks.length >= 10) {
+      alert(
+        "Maksymalna ilość zadań to 10. Jeśli chcesz dodać nowe zadanie musisz coś usunąć"
+      );
+      return;
+    }
+
+    function getValue() {
+      const input = document.querySelector(".input-add");
+      let inputValue = input.value;
+
+      if (inputValue === "") return;
+      input.value = "";
+      return inputValue;
+    }
+    const value = getValue();
+    let task;
+
+    if (value) {
+      task = new NewTask();
+      task.setTask(value);
+      task.setId(week[day].addTask(task));
+      generateHtml(task);
+      updateData();
+      showTasksCount(week);
+      $("#toast-added").toast("show");
+    }
+  };
+
+  const removeTask = function(e) {
+    e.preventDefault();
+    if (
+      e.target.classList.contains("delete-btn") ||
+      e.target.parentElement.classList.contains("delete-btn")
+    ) {
+      const element = e.target.closest("li");
+      const day = findActiveDay(week);
+      week[day].removeTask(element.dataset.id);
+
+      element.parentElement.removeChild(element);
+
+      $("#toast-removed").toast("show");
+
+      week[day].tasks.forEach((task, i) => {
+        task.setId(i);
+      });
+
+      updateDataId(week[day].tasks);
+      updateData();
+      showTasksCount(week);
+
+      if (!week[day].tasks.length) {
+        btnRemoveAll.style.display = "none";
+      }
+    }
+  };
+
+  const checkTask = function(e) {
+    e.preventDefault();
+    if (
+      e.target.classList.contains("check-btn") ||
+      e.target.parentElement.classList.contains("check-btn")
+    ) {
+      const element = e.target.closest("li");
+      const day = findActiveDay(week);
+      week[day].tasks[element.dataset.id].setDone(true);
+      if (e.target.classList.contains("check-btn"))
+        e.target.classList.add("btn-success");
+      else e.target.parentElement.classList.add("btn-success");
+      updateData();
+      showTasksCount(week);
+    }
+  };
+
+  const updateTask = function(e) {
+    e.preventDefault();
+    if (e.target.classList.contains("task-field")) {
+      const currentTaskId = e.target.closest("li").dataset.id;
+      const currentValue = e.target.value;
+      const day = findActiveDay(week);
+      week[day].tasks[currentTaskId].setTask(currentValue);
+      updateData();
+    }
+  };
+
+  const removeAllTasks = function(e) {
+    e.preventDefault();
+    const day = findActiveDay(week);
+    week[day].removeAllTasks();
+    updateData();
+
+    const elems = document.querySelectorAll(".single-task:not(#clone)");
+    elems.forEach(elem => {
+      toDoList.removeChild(elem);
+    });
+    if (!week[day].tasks.length) {
+      btnRemoveAll.style.display = "none";
+    }
+    showTasksCount(week);
+    $("#toast-removed-all").toast("show");
+  };
+
+  document.addEventListener("DOMContentLoaded", showContent);
+  document.querySelector("#btn-add").addEventListener("click", addNewTask);
+  toDoList.addEventListener("click", removeTask);
+  toDoList.addEventListener("click", checkTask);
+  toDoList.addEventListener("input", updateTask);
+  btnRemoveAll.addEventListener("click", removeAllTasks);
+  navItems.forEach(item => {
+    item.addEventListener("click", chooseDay);
+  });
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,52 @@
-const path = require('path');
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    watch: true,
-    devtool: "source-map",
-    module: {
-        rules: [{
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  watch: true,
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "assets",
+              name: "[name].[ext]"
             }
-        }]
-    }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader" // translates CSS into CommonJS
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([{ from: "./src/assets", to: "assets" }]),
+    new HtmlWebpackPlugin({ template: "./index.html" })
+  ]
 };
